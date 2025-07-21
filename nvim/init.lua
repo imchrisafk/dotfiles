1c71e0434a3eb2c4eb848ba82046561b27de3999
Chris <chrisafk@pm.me>
2025-07-20 23:51:23 -0400

    Added neovim config.

---
+++ nvim/init.lua
@@ -0,0 +1,27 @@
+vim.opt.number = true
+-- vim.opt.relativenumber = true
+
+vim.opt.mouse = "a"
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.cmd([[colorscheme wildcharm]])
+
+vim.opt.cursorline = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
+
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
