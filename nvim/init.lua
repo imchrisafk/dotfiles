324196c1dc3ed468ab7bab1972c707a533cc4021
Chris <chrisafk@pm.me>
2025-07-21 00:11:28 -0400

    Fully flushed out neovim config with some common sense options and plugins.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,141 @@
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Core settings
 vim.opt.number = true
--- vim.opt.relativenumber = true
-
+vim.opt.relativenumber = true
 vim.opt.mouse = "a"
-
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.autoindent = true
-
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
 vim.opt.termguicolors = true
-
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.wrap = false -- Changed for code; toggle for prose below
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.scrolloff = 8
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 vim.cmd([[colorscheme wildcharm]])
 
-vim.opt.cursorline = true
+-- Filetype-specific settings
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "markdown", "text" },
+    callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "en_us"
+    end,
+})
 
-vim.opt.signcolumn = "yes"
+-- Key mappings
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
 
-vim.opt.clipboard = "unnamedplus"
+-- Plugin manager: lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+    end
+    vim.opt.rtp:prepend(lazypath)
 
-vim.opt.wrap = true
-vim.opt.linebreak = true
-
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
+    -- Plugins
+    require("lazy").setup({
+        -- File explorer
+        {
+            "nvim-tree/nvim-tree.lua",
+            config = function()
+            require("nvim-tree").setup {}
+            vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+            end,
+        },
+        -- Fuzzy finder
+        {
+            "nvim-telescope/telescope.nvim",
+            dependencies = { "nvim-lua/plenary.nvim" },
+            config = function()
+            require("telescope").setup {}
+            vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Find files" })
+            vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Search text" })
+            end,
+        },
+        -- Syntax highlighting
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            config = function()
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = { "lua", "python", "javascript", "html", "css" },
+                highlight = { enable = true },
+                indent = { enable = true },
+            }
+            end,
+        },
+        -- LSP and autocompletion
+        {
+            "neovim/nvim-lspconfig",
+            dependencies = {
+                "hrsh7th/nvim-cmp",
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-buffer",
+                "hrsh7th/cmp-path",
+            },
+            config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            lspconfig.lua_ls.setup { capabilities = capabilities }
+            lspconfig.pyright.setup { capabilities = capabilities }
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Find references" })
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+            local cmp = require("cmp")
+            cmp.setup {
+                mapping = cmp.mapping.preset.insert {
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                          ["<C-Space>"] = cmp.mapping.complete(),
+                          ["<CR>"] = cmp.mapping.confirm { select = true },
+                          ["<C-e>"] = cmp.mapping.abort(),
+                },
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                },
+            }
+            end,
+        },
+        -- Statusline
+        {
+            "nvim-lualine/lualine.nvim",
+            dependencies = { "nvim-tree/nvim-web-devicons" },
+            config = function()
+            require("lualine").setup { options = { theme = "auto" } }
+            end,
+        },
+    })
