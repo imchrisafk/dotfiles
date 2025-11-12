8fac50cd21ab69b909b35870e12463d26699cf69
Chris <chrisafk@pm.me>
2025-11-12 11:38:03 -0500

    Made ln interactive if dest. file exists

--- install.sh
+++ install.sh
@@ -5,22 +5,21 @@
 # TODO:
 # - Ensure script runs properly irrespective of cd
 # - Use env vars where applicable
-# - Check if files exist, prompt user
 # - More verbose comments
 # - Some options, perhaps dry-run, verbose, etc
 
 # bash
-ln -vs ./bash/.alias ~/.alias
-ln -vs ./bash/.bashrc ~/.bashrc
+ln -vis ./bash/.alias ~/.alias
+ln -vis ./bash/.bashrc ~/.bashrc
 
 # mpv
 mkdir -vp ~/.config/mpv
-ln -vs ./mpv/input.conf ~/.config/mpv/input.conf
-ln -vs ./mpv/mpv.conf ~/.config/mpv/mpv.conf
+ln -vis ./mpv/input.conf ~/.config/mpv/input.conf
+ln -vis ./mpv/mpv.conf ~/.config/mpv/mpv.conf
 
 # nvim
 mkdir -vp ~/.config/nvim
-ln -vs ./nvim/init.lua ~/.config/nvim/init.lua
+ln -vis ./nvim/init.lua ~/.config/nvim/init.lua
 
 # starship
-ln -vs ./starship/starship.toml ~/.config/starship.toml
+ln -vis ./starship/starship.toml ~/.config/starship.toml
