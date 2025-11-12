a57f8b75ad7d37a39b2030fecc1e1b6bd6c19bcf
Chris <chrisafk@pm.me>
2025-11-12 13:31:41 -0500

    Added environment variables where appropriate

--- install.sh
+++ install.sh
@@ -1,37 +1,38 @@
 #!/usr/bin/env bash
 
 # Creates symlinks for the various configuration files included in https://codeberg.org/chrisafk/dotfiles
+# Ensures proper permissions on source files and target directories
 
 # TODO:
-# - Ensure script runs properly irrespective of cd
-# - Use env vars where applicable
 # - More verbose comments
 # - Some options, perhaps dry-run, etc
 
-# Get the absolute path to the directory containing this script
+# Paths
 REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+H_CFG="$HOME/.config"
+H_LOC_SHR="$HOME/.local/share"
 
 # bash
 ln -vis "$REPO_DIR/bash/.alias" "$HOME/.alias"
 ln -vis "$REPO_DIR/bash/.bashrc" "$HOME/.bashrc"
 
 # konsole
-mkdir -vp "$HOME/.local/share/konsole"
-ln -vis "$REPO_DIR/konsole/Linux.colorscheme" "$HOME/.local/share/konsole/Linux.colorscheme"
-ln -vis "$REPO_DIR/konsole/chrisafk.profile" "$HOME/.local/share/konsole/chrisafk.profile"
+mkdir -vp "$H_LOC_SHR/konsole"
+ln -vis "$REPO_DIR/konsole/Linux.colorscheme" "$H_LOC_SHR/konsole/Linux.colorscheme"
+ln -vis "$REPO_DIR/konsole/chrisafk.profile" "$H_LOC_SHR/konsole/chrisafk.profile"
 
-mkdir -vp "$HOME/.config"
-ln -vis "$REPO_DIR/konsole/konsolerc" "$HOME/.config/konsolerc"
+mkdir -vp "$H_CFG"
+ln -vis "$REPO_DIR/konsole/konsolerc" "$H_CFG/konsolerc"
 
 # mpv
-mkdir -vp "$HOME/.config/mpv"
-ln -vis "$REPO_DIR/mpv/input.conf" "$HOME/.config/mpv/input.conf"
-ln -vis "$REPO_DIR/mpv/mpv.conf" "$HOME/.config/mpv/mpv.conf"
+mkdir -vp "$H_CFG/mpv"
+ln -vis "$REPO_DIR/mpv/input.conf" "$H_CFG/mpv/input.conf"
+ln -vis "$REPO_DIR/mpv/mpv.conf" "$H_CFG/mpv/mpv.conf"
 
 # nvim
-mkdir -vp "$HOME/.config/nvim"
-ln -vis "$REPO_DIR/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+mkdir -vp "$H_CFG/nvim"
+ln -vis "$REPO_DIR/nvim/init.lua" "$H_CFG/nvim/init.lua"
 
 # starship
-mkdir -vp "$HOME/.config"
-ln -vis "$REPO_DIR/starship/starship.toml" "$HOME/.config/starship.toml"
+mkdir -vp "$H_CFG"
+ln -vis "$REPO_DIR/starship/starship.toml" "$H_CFG/starship.toml"
