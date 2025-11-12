af2ed249dcfc9bade456776d382db7f2633ea73d
Chris <chrisafk@pm.me>
2025-11-12 12:54:46 -0500

    Added konsole config files to install script

--- install.sh
+++ install.sh
@@ -12,6 +12,14 @@
 ln -vis ./bash/.alias ~/.alias
 ln -vis ./bash/.bashrc ~/.bashrc
 
+# konsole
+mkdir -vp ~/.local/share/konsole
+ln -vis ./konsole/Linux.colorscheme ~/.local/share/konsole/Linux.colorscheme
+ln -vis ./konsole/chrisafk.profile ~/.local/share/konsole/chrisafk.profile
+
+mkdir -vp ~/.config
+ln -vis ./konsole/konsolerc ~/.config/konsolerc
+
 # mpv
 mkdir -vp ~/.config/mpv
 ln -vis ./mpv/input.conf ~/.config/mpv/input.conf
@@ -22,4 +30,5 @@ mkdir -vp ~/.config/nvim
 ln -vis ./nvim/init.lua ~/.config/nvim/init.lua
 
 # starship
+mkdir -vp ~/.config
 ln -vis ./starship/starship.toml ~/.config/starship.toml
